d4ebd8408d8921f98b6a2af195804105e69592de
mahon victor <mahon.victor@gmail.com>
2021-02-07 17:49:08 +0100

    setup min api kestrel

---
+++ Controllers/WorkshopController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DockerWorkshop.Controllers
+{
+    [Route("api/v0/workshops")]
+    [ApiController]
+    public class WorkshopController : ControllerBase
+    {
+
+        const string yourTheFuture = @"
+                                      _   _             __       _                 
+ _   _  ___  _   _    __ _ _ __ ___  | |_| |__   ___   / _|_   _| |_ _   _ _ __ ___ 
+| | | |/ _ \| | | |  / _` | '__/ _ \ | __| '_ \ / _ \ | |_| | | | __| | | | '__/ _ \
+| |_| | (_) | |_| | | (_| | | |  __/ | |_| | | |  __/ |  _| |_| | |_| |_| | | |  __/
+ \__, |\___/ \__,_|  \__,_|_|  \___|  \__|_| |_|\___| |_|  \__,_|\__|\__,_|_|  \___|
+ |___/
+";
+
+        [HttpGet]
+        public IActionResult GetWorkshop()
+        {
+            return Ok(yourTheFuture);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace DockerWorkshop
 {
@@ -20,7 +14,11 @@ namespace DockerWorkshop
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseKestrel(serverOption => {
+                        serverOption.ListenLocalhost(8080);
+                    })
+                    .UseStartup<Startup>();    
                 });
     }
 }
